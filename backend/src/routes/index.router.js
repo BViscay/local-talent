772a60c59acfd2fba9afe26ea1e121e25c27af04
unknown <diaperezro@unal.edu.co>
2023-10-19 23:29:57 -0500

    Prueba git 2

--- backend/src/routes/index.router.js
+++ backend/src/routes/index.router.js
@@ -2,6 +2,6 @@ const { Router } = require('express')
 
 const router = Router()
 
-router.get('/', (req, res) => { res.send('hola mundo ( Diego )') })
+router.get('/', (req, res) => { res.send('hola mundo ( Diego1 )') })
 
 module.exports = router
