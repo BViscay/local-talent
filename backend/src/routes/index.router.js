fc6bc8b2ed7c3102fb4ba7f389b96117d57a1b73
unknown <diaperezro@unal.edu.co>
2023-10-21 08:13:37 -0500

    prueba gitflow 10/21/23

--- backend/src/routes/index.router.js
+++ backend/src/routes/index.router.js
@@ -2,6 +2,6 @@ const { Router } = require('express')
 
 const router = Router()
 
-router.get('/', (req, res) => { res.send('hola mundo ( Diego1 )') })
+router.get('/', (req, res) => { res.send('hola mundo ( Diego2 )') })
 
 module.exports = router
