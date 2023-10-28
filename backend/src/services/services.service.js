4ec44516b6184e3f1368c39c541e170aed13dcd5
Bruno Viscay <brunoviscay@gmail.com>
2023-10-28 19:43:46 -0300

    Update services.service.js
    data.categoryId = Number(data.categoryId) in line 10

--- backend/src/services/services.service.js
+++ backend/src/services/services.service.js
@@ -7,7 +7,7 @@ const createService = async (data, dataImg) => {
   const resultImage = await uploadImageCreate(dataImg)
 
   //! PROVISORIO
-  data.CategoryId = parseInt(data.CategoryId)
+  data.categoryId = Number(data.categoryId)
 
   const newService = await Service.create({
     ...data,
