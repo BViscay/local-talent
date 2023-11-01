eaa4e3de5bf2c3657b82abf8d4a0fac05f7324c2
Angel Tirado <angelteduardov@gmail.com>
2023-10-31 21:23:02 -0300

    Node_modules
    Carpeta Node_modules

--- backend/src/libs/handleRandom.js
+++
@@ -1,8 +0,0 @@
-function generateRandomNumber () {
-  const randomValue = Math.random()
-  const numberInRange = randomValue * 900000
-  const sixDigitNumber = Math.floor(numberInRange) + 100000
-  return sixDigitNumber
-}
-
-module.exports = { generateRandomNumber }
