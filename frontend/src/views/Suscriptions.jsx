424f02d82269ad6de4fdf63643d745feee0c0149
Angel Tirado <angelteduardov@gmail.com>
2023-11-02 12:39:16 -0300

    Cambio en el modelo product y en el componente de los botones de suscripcion

--- frontend/src/views/Suscriptions.jsx
+++ frontend/src/views/Suscriptions.jsx
@@ -1,22 +1,31 @@
 import useSuscriptions from "../hooks/useSuscriptions";
 
 export default function Suscriptions() {
-  const {handleSilverSuscription, handleGoldSuscription} = useSuscriptions();
+  const { handleSilverSuscription, handleGoldSuscription } = useSuscriptions();
 
   return (
-    <div className='flex w-full gap-5 items-center justify-center'>
-      <div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-center" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <p className="text-2xl font-bold mb-2">Nuestras suscripciones</p>
+        <div className="bg-primary-100 rounded-lg p-4 mb-4">
+          <p className="text-lg">
+            Oro: 10 publicaciones durante 3 meses
+          </p>
+        </div>
+        <div className="bg-primary-100 rounded-lg p-4 mb-4">
+          <p className="text-lg">
+            Plata: 5 publicaciones durante 2 meses
+          </p>
+        </div>
         <button
           onClick={handleSilverSuscription}
-          className='flex self-center items-center justify-center w-36 mr-2 -ml-4  text-white  font-medium rounded-lg text-lg p-1.5 text-center bg-primary-600 hover:bg-cyan-900'
+          className="w-36 text-white font-medium rounded-lg text-lg p-1.5 text-center bg-primary-600 hover-bg-cyan-900 mb-4"
         >
           Plata
         </button>
-      </div>
-      <div>
         <button
           onClick={handleGoldSuscription}
-          className='flex self-center items-center justify-center w-36 mr-2 -ml-4  text-white  font-medium rounded-lg text-lg p-1.5 text-center bg-primary-600 hover:bg-cyan-900'
+          className="w-36 text-white font-medium rounded-lg text-lg p-1.5 text-center bg-primary-600 hover-bg-cyan-900"
         >
           Oro
         </button>
