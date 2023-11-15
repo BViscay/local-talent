89ee92a6c8c289b03213b59cd4f3b7f34601642f
Bruno Viscay <brunoviscay@gmail.com>
2023-11-15 08:04:12 -0300

    moooore changes

--- frontend/src/components/Notification/RedPointNotification.jsx
+++ frontend/src/components/Notification/RedPointNotification.jsx
@@ -2,14 +2,15 @@ import {useSelector} from "react-redux";
 import {getNotifications} from "../../redux/sliceLogin";
 
 export default function RedPointNotification() {
-  const notifications = useSelector(getNotifications);
-  const activeNotifications = notifications?.length >= 1;
+  const getMyNotifications = useSelector(getNotifications);
+  const notifications = getMyNotifications.length;
+  const activeNotifications = notifications >= 1;
 
   return (
     activeNotifications && (
       <div className='flex justify-center items-center absolute w-[14px] h-[14px] rounded-full bg-red-600'>
         <p className='text-white text-[9px] font-bold pl-[1px]'>
-          {notifications.newNotifications}
+          {notifications}
         </p>
       </div>
     )
